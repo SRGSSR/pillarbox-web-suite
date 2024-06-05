2efcfd1dbbe82f12f9f6beae65148e89e8597eae
Josep Boix Requesens <jspboix@gmail.com>
2024-06-05 15:20:04 +0200

    fix: preserve default options when adding new translations
    Refactored language modules to merge with existing default translations in
    `videojs.options.language`. This prevents overriding default configurations when calling
    `videojs.addLanguage`. Changes have been applied across all packages in the monorepo and the
    templates for the generation of new packages.
    
    Other changes:
    - Removed spaces from the skip button translation keys.
    - Expose language files as assets into the final build.

--- packages/skip-button/vite.config.lib.js
+++ packages/skip-button/vite.config.lib.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import babel from '@rollup/plugin-babel';
+import copy from 'rollup-plugin-copy';
 
 /**
  * Vite's configuration for the lib build.
@@ -19,10 +20,16 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['@srgssr/pillarbox-web'],
-      plugins: [babel({
-        babelHelpers: 'bundled',
-        exclude: 'node_modules/**'
-      })]
+      plugins: [
+        babel({
+          babelHelpers: 'bundled',
+          exclude: 'node_modules/**'
+        }),
+        copy({
+          targets: [{ src: 'src/lang/*.json', dest: 'dist/lang' }],
+          hook: 'writeBundle'
+        })
+      ]
     }
   }
 });
