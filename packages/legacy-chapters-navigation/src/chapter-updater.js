9d11e71851fbe952e73a8ee7c4b534531edabbb3
amtins <amtins.dev@gmail.com>
2026-01-16 22:21:20 +0100

    feat: handle chapter display on loadstart

--- packages/legacy-chapters-navigation/src/chapter-updater.js
+++ packages/legacy-chapters-navigation/src/chapter-updater.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+// cspell: disable
+
 import pillarbox from '@srgssr/pillarbox-web';
 
 /**
@@ -9,6 +12,7 @@ const Component = pillarbox.getComponent('Component');
 class ChapterUpdater extends Component {
   chapterUpdateInterval;
   cachedUrn;
+  isLive;
 
   constructor(player, options, ready) {
     const opts = pillarbox.obj.merge(
@@ -19,8 +23,15 @@ class ChapterUpdater extends Component {
 
     super(player, opts, ready);
 
+    this.handleChapterInitialization = this.handleChapterInitialization.bind(this);
     this.handleStartLongPolling = this.handleStartLongPolling.bind(this);
     this.handleStopLongPolling = this.handleStopLongPolling.bind(this);
+
+    this.on(
+      player,
+      'loadstart',
+      this.handleChapterInitialization
+    );
 
     this.on(
       player,
@@ -35,14 +46,67 @@ class ChapterUpdater extends Component {
     );
   }
 
+  /**
+   * @TODO: remove once https://github.com/SRGSSR/pillarbox-web/pull/362 is merged
+   */
+  async createTextTrack() {
+    const trackId = 'srgssr-chapters';
+    const removeTrack = this.player().textTracks().getTrackById(trackId);
+
+    if (removeTrack) {
+      this.player().textTracks().removeTrack(removeTrack);
+    }
+
+    const textTrack = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(new pillarbox.TextTrack({
+          id: trackId,
+          kind: 'metadata',
+          label: trackId,
+          tech: this.player().tech(true),
+        }));
+      }, 100);
+    });
+
+    this.player().textTracks().addTrack(textTrack);
+
+    return textTrack;
+  }
+
+  async handleChapterInitialization() {
+    const {
+      mediaData: {
+        urn,
+        live
+      } = {}
+    } = this.player().currentSource();
+
+    this.cachedUrn = urn;
+    this.isLive = live;
+
+    // VOD
+    if (!this.isLive) return;
+
+    const chapterTrack = await this.createTextTrack();
+    const chapterList = await this.fetchChaptersList(this.cachedUrn) || [];
+
+    chapterList.forEach(chapter => {
+      this.player()
+        .legacyChaptersNavigation.addChapter(chapterTrack, chapter)
+    });
+
+    if (chapterList.length) {
+      this.player()
+        .legacyChaptersNavigation.chaptersBar.show();
+    }
+
+  }
+
   handleStartLongPolling() {
     this.clearInterval(this.chapterUpdateInterval);
 
-    const currentSource = this.player().currentSource();
-
-    this.cachedUrn = currentSource
-      && currentSource.mediaData
-      && currentSource.mediaData.urn;
+    // VOD
+    if (!this.isLive) return;
 
     this.chapterUpdateInterval = this.setInterval(() => {
       const currentSource = this.player().currentSource();
@@ -61,37 +125,51 @@ class ChapterUpdater extends Component {
     this.clearInterval(this.chapterUpdateInterval);
   }
 
-  async update(urn) {
-    const legacyChaptersNavigation = this.player()
-      .legacyChaptersNavigation;
-
-    if (
-      !legacyChaptersNavigation ||
-      !legacyChaptersNavigation.chaptersBar
-    ) return;
-
+  async fetchChaptersList(urn) {
     const { chapterList } = await this.player()
       .options()
       .srgOptions
       .dataProvider(urn);
 
+    return chapterList
+      .filter(chapter => chapter.urn !== this.cachedUrn);
+  }
+
+  async update(urn) {
+    const legacyChaptersNavigation = this.player()
+      .legacyChaptersNavigation;
+    const srgssrChaptersTrack = this.player().textTracks().getTrackById('srgssr-chapters');
+
+    if (
+      !srgssrChaptersTrack ||
+      !legacyChaptersNavigation ||
+      !legacyChaptersNavigation.chaptersBar
+    ) return;
+
+    const chapterList = await this.fetchChaptersList(urn);
     const chaptersUrn = legacyChaptersNavigation
       .chaptersBar
       .chapters()
       .map(chapter => chapter.metadata.urn);
-
     const chaptersToAdd = chapterList
       .filter(chapter =>
         !chaptersUrn.includes(chapter.urn) &&
         chapter.urn !== this.cachedUrn);
 
     chaptersToAdd.forEach(chapter => {
-      legacyChaptersNavigation.addChapter(chapter);
+      legacyChaptersNavigation.addChapter(srgssrChaptersTrack, chapter);
     });
   }
 
   dispose() {
     this.clearInterval(this.chapterUpdateInterval);
+
+    this.off(
+      this.player(),
+      'loadstart',
+      this.handleChapterInitialization
+    );
+
     this.off(
       this.player(),
       'loadeddata',
