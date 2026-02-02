94c0ba3bfe95437a79298bb70b15682bb7d4646e
amtins <amtins.dev@gmail.com>
2026-02-02 19:31:11 +0100

    feat: add update or chapter deletion
    When the metadata is updated, this is reflected on the affected chapter(s),
    either by updating the data of the chapter(s) or by deleting the chapter(s).

--- packages/legacy-chapters-navigation/src/chapter-updater.js
+++ packages/legacy-chapters-navigation/src/chapter-updater.js
@@ -11,7 +11,7 @@ const Component = pillarbox.getComponent('Component');
 
 class ChapterUpdater extends Component {
   chapterUpdateInterval;
-  cachedUrn;
+  cachedMainChapterUrn;
   isLive;
 
   constructor(player, options, ready) {
@@ -46,7 +46,7 @@ class ChapterUpdater extends Component {
     );
   }
 
-  async handleChapterInitialization({ track : chapterTrack }) {
+  async handleChapterInitialization({ track: chapterTrack }) {
     if (chapterTrack.id !== 'srgssr-chapters') return;
 
     const {
@@ -56,13 +56,13 @@ class ChapterUpdater extends Component {
       } = {}
     } = this.player().currentSource();
 
-    this.cachedUrn = urn;
+    this.cachedMainChapterUrn = urn;
     this.isLive = live;
 
     // VOD
     if (!this.isLive) return;
 
-    const chapterList = await this.fetchChaptersList(this.cachedUrn) || [];
+    const chapterList = await this.fetchChaptersList(this.cachedMainChapterUrn) || [];
 
     chapterList.forEach(chapter => {
       this.player()
@@ -73,7 +73,6 @@ class ChapterUpdater extends Component {
       this.player()
         .legacyChaptersNavigation.chaptersBar.show();
     }
-
   }
 
   handleStartLongPolling() {
@@ -85,10 +84,10 @@ class ChapterUpdater extends Component {
       if (
         !currentSource ||
         !currentSource.mediaData ||
-        currentSource.mediaData.urn !== this.cachedUrn
+        currentSource.mediaData.urn !== this.cachedMainChapterUrn
       ) return;
 
-      this.update(this.cachedUrn);
+      this.update(this.cachedMainChapterUrn);
     }, this.options().pollingDelay);
   }
 
@@ -96,6 +95,9 @@ class ChapterUpdater extends Component {
     this.clearInterval(this.chapterUpdateInterval);
   }
 
+  /**
+   * Returns a chapter list without the main chapter
+   */
   async fetchChaptersList(urn) {
     const { chapterList } = await this.player()
       .options()
@@ -103,37 +105,224 @@ class ChapterUpdater extends Component {
       .dataProvider(urn);
 
     return chapterList
-      .filter(chapter => chapter.urn !== this.cachedUrn);
+      .filter(chapter => chapter.urn !== this.cachedMainChapterUrn);
   }
 
-  async update(urn) {
+  hasChaptersNavigation() {
     const legacyChaptersNavigation = this.player()
       .legacyChaptersNavigation;
     const srgssrChaptersTrack = this.player().textTracks().getTrackById('srgssr-chapters');
 
-    if (
-      !srgssrChaptersTrack ||
-      !legacyChaptersNavigation ||
-      !legacyChaptersNavigation.chaptersBar
-    ) return;
+    return Boolean(srgssrChaptersTrack) &&
+      Boolean(legacyChaptersNavigation) &&
+      Boolean(legacyChaptersNavigation.chaptersBar)
+  }
 
+  async update(urn) {
+    if (!this.hasChaptersNavigation()) return;
+
+    const { legacyChaptersNavigation } = this.player();
+    const srgssrChaptersTrack = this.player().textTracks().getTrackById('srgssr-chapters');
     const chapterList = await this.fetchChaptersList(urn);
-    const chaptersUrn = legacyChaptersNavigation
-      .chaptersBar
+
+    this.addChapters(
+      legacyChaptersNavigation,
+      srgssrChaptersTrack,
+      chapterList
+    );
+
+    this.updateChapters(
+      legacyChaptersNavigation,
+      urn,
+      srgssrChaptersTrack,
+      chapterList
+    );
+
+    this.removeChapters(
+      legacyChaptersNavigation,
+      srgssrChaptersTrack,
+      chapterList
+    );
+
+    if (!srgssrChaptersTrack.cues.length) {
+      legacyChaptersNavigation.chaptersBar.children().length = 0;
+      legacyChaptersNavigation.handleChapterVisibility();
+    }
+  }
+
+  addChapters(chapterNavigationComponent, chaptersTrack, chapterList = []) {
+    const { chaptersBar } = chapterNavigationComponent;
+    const chaptersUrn = chaptersBar
       .chapters()
       .map(chapter => chapter.metadata.urn);
-    const chaptersToAdd = chapterList
-      .filter(chapter =>
-        !chaptersUrn.includes(chapter.urn) &&
-        chapter.urn !== this.cachedUrn);
 
-    chaptersToAdd.forEach(chapter => {
-      legacyChaptersNavigation.addChapter(srgssrChaptersTrack, chapter);
+    const chaptersToAdd = chapterList
+      .reduce((chapterEntries, chapter, index) => {
+        if (chaptersUrn.includes(chapter.urn) ||
+          chapter.urn === this.cachedMainChapterUrn) return chapterEntries;
+
+        chapterEntries.push({
+          index,
+          chapter
+        });
+
+        return chapterEntries;
+      }, []);
+
+    chaptersToAdd.forEach(chapterEntry => {
+      this.addChapter(
+        chapterNavigationComponent,
+        chaptersTrack,
+        chapterEntry.chapter,
+        chapterEntry.index
+      );
+
+      chaptersBar.scrollToSelectedChapter(chaptersBar.children().at(-1), true);
     });
 
-    if(chapterList.length && legacyChaptersNavigation.chaptersBar.hasClass('vjs-hidden')) {
-      legacyChaptersNavigation.chaptersBar.show();
+    if (chapterList.length && chaptersBar.hasClass('vjs-hidden')) {
+      chaptersBar.show();
     }
+
+    if (!this.player().hasClass('pbw-with-chapters')) {
+      chapterNavigationComponent.handleChapterVisibility();
+    }
+
+    chapterNavigationComponent.updateButtons();
+  }
+
+  async addChapter(chapterNavigationComponent, chaptersTrack, chapter, chapterIndex) {
+    const startTime = (Number.isFinite(chapter.markIn)
+      ? chapter.markIn : chapter.fullLengthMarkIn) / 1_000;
+    const endTime = (Number.isFinite(chapter.markOut)
+      ? chapter.markOut : chapter.fullLengthMarkOut) / 1_000;
+
+    this.addChapterCue(chaptersTrack, startTime, endTime, chapter);
+    this.addChapterChild(chapterNavigationComponent, startTime, endTime, chapter, chapterIndex);
+  }
+
+  addChapterChild(chapterNavigationComponent, startTime, endTime, metadata, chapterIndex) {
+    chapterNavigationComponent.chaptersBar.addChapter(
+      {
+        startTime,
+        endTime,
+        metadata
+      },
+      chapterIndex
+    );
+  }
+
+  addChapterCue(chaptersTrack, startTime, endTime, chapter) {
+    const text = JSON.stringify(chapter);
+    const cue = new VTTCue(startTime, endTime, text);
+
+    cue.id = chapter.urn;
+
+    chaptersTrack.addCue(cue);
+  }
+
+  updateChapters(chapterNavigationComponent, urn, chaptersTrack, chapterList = []) {
+    chapterList.forEach(chapter => {
+      // main chapter or unrelated chapter
+      if (chapter.urn === urn || chapter.fullLengthUrn !== urn) return;
+
+      const chapterStringified = JSON.stringify(chapter);
+      const startTime = (Number.isFinite(chapter.markIn)
+        ? chapter.markIn : chapter.fullLengthMarkIn) / 1_000;
+      const endTime = (Number.isFinite(chapter.markOut)
+        ? chapter.markOut : chapter.fullLengthMarkOut) / 1_000;
+
+      // chapter hasn't change
+      if (!this.updateChaptersCue(
+        chaptersTrack,
+        startTime,
+        endTime,
+        chapterStringified,
+        chapter
+      )) return;
+
+      this.updateChaptersChild(chapterNavigationComponent, chapter, startTime);
+
+    });
+  }
+
+  updateChaptersCue(chaptersTrack, startTime, endTime, chapterText, chapter) {
+    const cue = chaptersTrack.cues.getCueById(chapter.urn);
+
+    if (
+      cue.text === chapterText &&
+      cue.startTime === startTime &&
+      cue.endTime === endTime
+    ) return false;
+
+    cue.startTime = startTime;
+    cue.endTime = endTime;
+    cue.text = chapterText;
+
+    return true;
+  }
+
+  updateChaptersChild(chapterNavigationComponent, chapter, startTime) {
+    if (
+      !chapterNavigationComponent ||
+      !chapterNavigationComponent.chaptersBar
+    ) return;
+
+    const child = chapterNavigationComponent.chaptersBar.getChildById(chapter.urn);
+
+    // no child found
+    if (!child) return false;
+
+    child.$('.vjs-card-title').textContent = chapter.title;
+    child.$('.vjs-card-duration').textContent = pillarbox.formatTime(
+      chapter.duration / 1_000, 600
+    );
+    child.$('.vjs-card-img').src = chapter.imageUrl;
+    child.options({
+      clickHandler: ([e]) => {
+        e.preventDefault();
+
+        this.player().currentTime(startTime + 0.1);
+      }
+    });
+
+    return true;
+  }
+
+  removeChapters(chapterNavigationComponent, chaptersTrack, chapterList = []) {
+    if (
+      !chapterNavigationComponent ||
+      !chapterNavigationComponent.chaptersBar
+    ) return;
+
+    const cues = Array.from(chaptersTrack.cues);
+
+    // no chapter
+    if (!cues.length) return;
+
+    // remove everything
+    if (cues.length && !chapterList.length) {
+      Array.from(cues).forEach(cue => {
+        const chapter = chapterNavigationComponent.chaptersBar.getChildById(cue.id);
+
+        chapter.dispose();
+        chapterNavigationComponent.chaptersBar.removeChild(chapter);
+        chaptersTrack.removeCue(cue);
+      });
+
+      return;
+    }
+
+    // specific chapter
+    cues.forEach(cue => {
+      if (chapterList.map(chapter => chapter.urn).includes(cue.id)) return;
+
+      chaptersTrack.removeCue(cue);
+
+      const chapter = chapterNavigationComponent.chaptersBar.getChildById(cue.id);
+      chapter.dispose();
+      chapterNavigationComponent.chaptersBar.removeChild(chapter);
+    });
   }
 
   dispose() {
